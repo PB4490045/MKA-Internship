3cbf2db0d9a2b0c82dc7b05abe1754a032f6b20e
PB4490045 <p.b.vandenberg@student.tudelft.nl>
2024-11-10 17:05:05 +0100

    Stap verder maar klaar met meneer krabs naast me

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ import os
 # Main function
 
 # Define input and output folders
-groundtruth_folder = r'Z:\TM Internships\Dept of CMF\Bram Roumen\Master Thesis - CMF\Thesis\Ground Truth database\Nifti - Segmentaties met landmarks'
-predicted_folder   = r'Z:\\TM Internships\Dept of CMF\Bram Roumen\Master Thesis - CMF\Thesis\Inference workflow\Predicted patients'
-# input_folder      = r'C:\Users\pb_va\OneDrive\Documents\Technical Medicine\TM2 - Stage 1 - MKA chirurgie\Bram Roumen\Inference workflow\Predicted patients'
+# groundtruth_folder = r'Z:\TM Internships\Dept of CMF\Bram Roumen\Master Thesis - CMF\Thesis\Ground Truth database\Nifti - Segmentaties met landmarks'
+# predicted_folder   = r'Z:\\TM Internships\Dept of CMF\Bram Roumen\Master Thesis - CMF\Thesis\Inference workflow\Predicted patients'
+groundtruth_folder = r'C:\Users\pb_va\OneDrive\Documents\Technical Medicine\TM2 - Stage 1 - MKA chirurgie\Bram Roumen\Ground Truth database\Nifti - Segmentaties met landmarks'
+predicted_folder   = r'C:\Users\pb_va\OneDrive\Documents\Technical Medicine\TM2 - Stage 1 - MKA chirurgie\Bram Roumen\Inference workflow\Predicted patients'
+
 output_folder      = 'Output'
 
 # Create paths
@@ -20,19 +22,19 @@ groundtruth_path, predicted_path, output_path = pp.create_paths(groundtruth_fold
 # create_folders(input_path, output_path)
 
 # # Create dataframes of landmarks
-# df_pred, folder_names = pp.create_dataframe(predicted_path)
-# df_gt = pp.create_dataframe_from_folders(groundtruth_path, folder_names)
+df_pred, folder_names = pp.create_dataframe(predicted_path)
+df_gt = pp.create_dataframe_from_folders(groundtruth_path, folder_names)
 
-# df_groundtruth = pp.create_planes(df_gt)
-# df_predicted = pp.create_planes(df_pred)
+df_groundtruth = pp.create_planes(df_gt)
+df_predicted = pp.create_planes(df_pred)
 
 # # # Save dataframes
 # pp.save_df(df_groundtruth, output_path, 'df_groundtruth')
 # pp.save_df(df_predicted, output_path, 'df_predicted')
 
-# Load dataframes for testing
-df_groundtruth = pp.loadcsv(output_path, 'df_groundtruth.csv')
-df_predicted = pp.loadcsv(output_path, 'df_predicted.csv')
+# # Load dataframes for testing
+# df_groundtruth = pp.loadcsv(output_path, 'df_groundtruth.csv')
+# df_predicted = pp.loadcsv(output_path, 'df_predicted.csv')
 
 # # Perform Cephalometric Analysis
 ceph_gt = ca.cephalometric_analysis(df_groundtruth)
